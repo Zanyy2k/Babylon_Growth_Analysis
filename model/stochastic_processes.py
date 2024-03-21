7110541487edbc477f7216dd9a0230bf98640b76
ZD <yuzongdongsg@hotmail.com>
2024-03-21 21:16:15 +0800

    modified the code to make it more efficient

--- model/stochastic_processes.py
+++ model/stochastic_processes.py
@@ -10,6 +10,7 @@ import model.constants as constants
 import experiments.simulation_configuration as simulation
 from experiments.utils import rng_generator
 
+
 def create_convex_polygn_price_process(
     timesteps=simulation.TIMESTEPS,
     dt=simulation.DELTA_TIME,
@@ -17,16 +18,25 @@ def create_convex_polygn_price_process(
     minimum_polygn_price=1,
 ):
     maximum_polygn_price = 10
-    #linear
-    samples = [minimum_polygn_price + (10-minimum_polygn_price)*i/(timesteps * dt + 2) for i in range((timesteps * dt + 2))]
+    # linear
+    samples = [
+        minimum_polygn_price + (10 - minimum_polygn_price) * i / (timesteps * dt + 2)
+        for i in range((timesteps * dt + 2))
+    ]
     # qudratic convex
-    t = timesteps*dt
-    para = (maximum_polygn_price-minimum_polygn_price)/((t+1)**2)
-    samples = [para * (i**2)+minimum_polygn_price for i in range(t+2)]
+    t = timesteps * dt
+    para = (maximum_polygn_price - minimum_polygn_price) / ((t + 1) ** 2)
+    samples = [para * (i**2) + minimum_polygn_price for i in range(t + 2)]
     # Ensure that the average token price is set at $5
-    curr_average = sum(samples)/len(samples)-minimum_polygn_price
+    curr_average = sum(samples) / len(samples) - minimum_polygn_price
     target_avg = 5
-    samples = [(sample-minimum_polygn_price) / curr_average * (target_avg -minimum_polygn_price)+ minimum_polygn_price for sample in samples] 
+    samples = [
+        (sample - minimum_polygn_price)
+        / curr_average
+        * (target_avg - minimum_polygn_price)
+        + minimum_polygn_price
+        for sample in samples
+    ]
     return samples
 
 
@@ -45,7 +55,7 @@ def create_stochastic_polygn_price_process(
     maximum_polygn_price = 10
     # Brownian Motion
     process = processes.continuous.BrownianExcursion(t=(timesteps * dt), rng=rng)
-    #process = processes.continuous.BrownianMotion(t=(timesteps * dt), rng=rng)
+    # process = processes.continuous.BrownianMotion(t=(timesteps * dt), rng=rng)
     samples = process.sample(timesteps * dt + 1)
     maximum_polygn_price_in_samples = max(samples)
     samples = [
@@ -53,15 +63,21 @@ def create_stochastic_polygn_price_process(
         for polygn_price_sample in samples
     ]
     # convex curve
-    t = timesteps*dt
-    para = (maximum_polygn_price-minimum_polygn_price)/((t+1)**2)
-    samples_convex = [para * (i**2)+minimum_polygn_price for i in range(t+2)]
+    t = timesteps * dt
+    para = (maximum_polygn_price - minimum_polygn_price) / ((t + 1) ** 2)
+    samples_convex = [para * (i**2) + minimum_polygn_price for i in range(t + 2)]
     # two samples addition
-    samples = [i+j for i,j in zip(samples_convex, samples)]
+    samples = [i + j for i, j in zip(samples_convex, samples)]
     # Ensure that the average token price is set at $5
-    curr_average = sum(samples)/len(samples)-minimum_polygn_price
+    curr_average = sum(samples) / len(samples) - minimum_polygn_price
     target_avg = 5
-    samples = [(sample-minimum_polygn_price) / curr_average * (target_avg -minimum_polygn_price)+ minimum_polygn_price for sample in samples]
+    samples = [
+        (sample - minimum_polygn_price)
+        / curr_average
+        * (target_avg - minimum_polygn_price)
+        + minimum_polygn_price
+        for sample in samples
+    ]
     return samples
 
 
@@ -71,10 +87,10 @@ def create_exp_adoption_rate(
     rng=np.random.default_rng(1),
     final_chains_num=1000,
 ):
-    t = timesteps*dt
-    para = (final_chains_num-2)/(t**2)
-    rates = [int(para * (dt*i)**2) for i in range(timesteps+1)]
-    rates = [j-i for i, j in zip(rates[:-1], rates[1:])]
+    t = timesteps * dt
+    para = (final_chains_num - 2) / (t**2)
+    rates = [int(para * (dt * i) ** 2) for i in range(timesteps + 1)]
+    rates = [j - i for i, j in zip(rates[:-1], rates[1:])]
     rates = [rate for rate in rates for _ in range(dt)]
     return rates
 
@@ -108,11 +124,14 @@ def create_moore_s_law_hardware_cost(
 ):
     ## Moore's Law
     # transit dt to real year
-    t = timesteps*dt
-    final_year = t/constants.epochs_per_year
+    t = timesteps * dt
+    final_year = t / constants.epochs_per_year
     # Moore's Law
     moore_low_year_half_period = 3
-    hardware_cost = [init_hardware_cost/(2**(i/t*final_year/moore_low_year_half_period)) for i in range(t)]
+    hardware_cost = [
+        init_hardware_cost / (2 ** (i / t * final_year / moore_low_year_half_period))
+        for i in range(t)
+    ]
     return hardware_cost
 
 
@@ -122,7 +141,7 @@ def create_stochastic_process_realizations(
     dt=simulation.DELTA_TIME,
     runs=5,
     final_chains_num=1000,
-    init_hardware_cost = 500,
+    init_hardware_cost=500,
 ):
     """Create stochastic process realizations
 
@@ -132,15 +151,24 @@ def create_stochastic_process_realizations(
 
     switcher = {
         "convex_polygn_price_samples": [
-            create_convex_polygn_price_process(timesteps=timesteps, dt=dt, rng=rng_generator())
+            create_convex_polygn_price_process(
+                timesteps=timesteps, dt=dt, rng=rng_generator()
+            )
             for _ in range(runs)
         ],
         "stochastic_polygn_price_samples": [
-            create_stochastic_polygn_price_process(timesteps=timesteps, dt=dt, rng=rng_generator())
+            create_stochastic_polygn_price_process(
+                timesteps=timesteps, dt=dt, rng=rng_generator()
+            )
             for _ in range(runs)
         ],
         "adoption_rates": [
-            create_exp_adoption_rate(timesteps=timesteps, dt=dt, rng=rng_generator(), final_chains_num=final_chains_num)
+            create_exp_adoption_rate(
+                timesteps=timesteps,
+                dt=dt,
+                rng=rng_generator(),
+                final_chains_num=final_chains_num,
+            )
             for _ in range(runs)
         ],
         "validator_samples": [
@@ -151,7 +179,12 @@ def create_stochastic_process_realizations(
             rng_generator().uniform(0.96, 0.99, timesteps * dt + 1) for _ in range(runs)
         ],
         "hardware_costs": [
-            create_moore_s_law_hardware_cost(timesteps=timesteps, dt=dt, rng=rng_generator(), init_hardware_cost=init_hardware_cost)
+            create_moore_s_law_hardware_cost(
+                timesteps=timesteps,
+                dt=dt,
+                rng=rng_generator(),
+                init_hardware_cost=init_hardware_cost,
+            )
             for _ in range(runs)
         ],
     }
@@ -159,33 +192,41 @@ def create_stochastic_process_realizations(
     return switcher.get(process, "Invalid Process")
 
 
-def create_intial_state_risk_service_validator(public_chain_cnt,private_chain_cnt, validator_cnt):
+def create_intial_state_risk_service_validator(
+    public_chain_cnt, private_chain_cnt, validator_cnt
+):
     chain_cnt = public_chain_cnt + private_chain_cnt
     p = simulation.p_service(chain_cnt)
     for i in range(public_chain_cnt):
-        p[i]=1 # 100% of validators would stake on public chains
+        p[i] = 1  # 100% of validators would stake on public chains
     for i in range(private_chain_cnt):
-        p[public_chain_cnt+i]=0.15 # 15% of validators would stake on public chains
+        p[public_chain_cnt + i] = 0.15  # 15% of validators would stake on public chains
 
     ## Randomize staking metrics for restaking (MultiStaking) mode
     # Initialize an empty matrix
     matrix_restaking = np.zeros((chain_cnt, validator_cnt))
-    
+
     # Fill the matrix with Bernoulli trials
     for i in range(chain_cnt):
         n = simulation.n_user(validator_cnt)
         for j in range(validator_cnt):
-            matrix_restaking[i][j] = np.random.binomial(1, p[i]) * n[j]    
+            matrix_restaking[i][j] = np.random.binomial(1, p[i]) * n[j]
     # Ensure at least 10% of entries in each row are non-zero
     for i in range(chain_cnt):
         non_zero_cnt = 6  # each service should have at least 6 validators
-        zero_indices = np.where(matrix_restaking[i] != 0)[0]  # indices where the row is zero
+        zero_indices = np.where(matrix_restaking[i] != 0)[
+            0
+        ]  # indices where the row is zero
         if len(zero_indices) < non_zero_cnt:
-            #non_zero_cnt = len(zero_indices)  # adjust if there are fewer than 10% zeros
+            # non_zero_cnt = len(zero_indices)  # adjust if there are fewer than 10% zeros
             matrix_restaking[i] = 0
-            non_zero_indices = np.random.choice(list(range(len(matrix_restaking[i]))), non_zero_cnt, replace=False)  # random select zero indices
+            non_zero_indices = np.random.choice(
+                list(range(len(matrix_restaking[i]))), non_zero_cnt, replace=False
+            )  # random select zero indices
             n = simulation.n_user(non_zero_cnt)
-            matrix_restaking[i][non_zero_indices] = n  # replace selected zeros with non-zero values
+            matrix_restaking[i][
+                non_zero_indices
+            ] = n  # replace selected zeros with non-zero values
     ## Randomize staking metrics for liquidity fragmentation (SingleStaking) mode
     # Initialize an empty matrix
     sum_per_node = matrix_restaking.sum(axis=0)
@@ -195,4 +236,4 @@ def create_intial_state_risk_service_validator(public_chain_cnt,private_chain_cn
     # print(np.any(sum_per_node == 0))  # Check for zeros
     # print(np.any(np.isnan(sum_per_node)))  # Check for NaNs
     matrix_liquidity_fragmentation = matrix_restaking / sum_per_node[np.newaxis, :]
-    return p, matrix_restaking/100, matrix_liquidity_fragmentation
+    return p, matrix_restaking / 100, matrix_liquidity_fragmentation
--- new_economic.py
+++ new_economic.py
@@ -4,6 +4,7 @@
 
 import copy
 
+import threading
 import model.constants as constants
 from model.stochastic_processes import create_stochastic_process_realizations
 from experiments.default_experiment import experiment
@@ -11,33 +12,30 @@ from experiments.default_experiment import experiment
 # Make a copy of the default experiment to avoid mutation
 experiment = copy.deepcopy(experiment)
 
+print(experiment)
+
+
 DELTA_TIME = constants.epochs_per_week  # epochs per timestep
 SIMULATION_TIME_DAYS = 10*365  # number of months
 TIMESTEPS = constants.epochs_per_day * SIMULATION_TIME_DAYS // DELTA_TIME
 
-# DELTA_TIME = 3
-# TIMESTEPS = 10
-
 # Generate stochastic process realizations
 ## Linear
 polygn_price_samples = create_stochastic_process_realizations("convex_polygn_price_samples", timesteps=TIMESTEPS, dt=DELTA_TIME)
-
 adoption_rates_slow = create_stochastic_process_realizations("adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=500)
 adoption_rates_med = create_stochastic_process_realizations("adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=2000)
 adoption_rates_fast = create_stochastic_process_realizations("adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=3500)
-
-
 adoption_rates_public_slow = create_stochastic_process_realizations("adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=5)
 adoption_rates_public_med = create_stochastic_process_realizations("adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=15)
 adoption_rates_public_fast = create_stochastic_process_realizations("adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=25)
-
 hardware_cost_moores_law = create_stochastic_process_realizations("hardware_costs", timesteps=TIMESTEPS, dt=DELTA_TIME, init_hardware_cost=500)
 
+# print(' polygn_price_samples : ', polygn_price_samples)
+print('polygn_price_samples_len : ', len(polygn_price_samples))
 
 parameter_overrides = {
     "polygn_price_process": [
         lambda run, timestep: polygn_price_samples[run - 1][timestep],
-        #lambda run, timestep: 1,
         ],
     'Adoption_speed_process': [
         lambda run, timestep: adoption_rates_slow[run - 1][timestep],
