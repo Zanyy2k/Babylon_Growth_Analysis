e16e83eb8dbd275d1d2729f1921fe1550aaf1805
ZD <yuzongdongsg@hotmail.com>
2024-03-22 22:18:07 +0800

    added a new page

---
+++ pages/Maintain_APY_Policy.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# import copy
+# import plotly.graph_objects as go
+
+# import experiments.visualizations as visualizations
+# import new_economic as new_adoption
+# from experiments.run import run
+
+### Main Window
+
+st.markdown(
+    "- **Maintain APY Policy**: To keep the Annual Percentage Yield (APY) for stakers around 4%. The incentives are adjusted to maintain the current stakers' APY at approximately 4%. There is no provision to attract new stakers unless the APY is increased above this threshold."
+)
