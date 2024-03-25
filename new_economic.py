d7afacfd7f59603f8bfb8fc6c969edeafcf16672
ZD <yuzongdongsg@hotmail.com>
2024-03-25 15:03:56 +0800

    modify the logic on calling model simulator and run

--- new_economic.py
+++ new_economic.py
@@ -2,74 +2,84 @@
 # Cumulative Yield Analysis
 """
 
-import copy
-
-import model.constants as constants
 from model.stochastic_processes import create_stochastic_process_realizations
-from experiments.default_experiment import experiment
-
-# Make a copy of the default experiment to avoid mutation
-experiment = copy.deepcopy(experiment)
-print("experiment : ", experiment.get_simulations())
+import time
 
 
-DELTA_TIME = constants.epochs_per_week  # epochs per timestep
-SIMULATION_TIME_DAYS = 5 * 365  # number of months
-TIMESTEPS = constants.epochs_per_day * SIMULATION_TIME_DAYS // DELTA_TIME
+def generate_stochastic_processes(TIMESTEPS, DELTA_TIME):
 
-# Generate stochastic process realizations
-## Linear
-polygn_price_samples = create_stochastic_process_realizations(
-    "convex_polygn_price_samples", timesteps=TIMESTEPS, dt=DELTA_TIME
-)
-adoption_rates_slow = create_stochastic_process_realizations(
-    "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=500
-)
-adoption_rates_med = create_stochastic_process_realizations(
-    "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=2000
-)
-adoption_rates_fast = create_stochastic_process_realizations(
-    "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=3500
-)
-adoption_rates_public_slow = create_stochastic_process_realizations(
-    "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=5
-)
-adoption_rates_public_med = create_stochastic_process_realizations(
-    "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=15
-)
-adoption_rates_public_fast = create_stochastic_process_realizations(
-    "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=25
-)
-hardware_cost_moores_law = create_stochastic_process_realizations(
-    "hardware_costs", timesteps=TIMESTEPS, dt=DELTA_TIME, init_hardware_cost=500
-)
+    print("Starting of generate_stochastic_processes...")
+    start_time = time.time()
 
-# print(' polygn_price_samples : ', polygn_price_samples)
-print("polygn_price_samples_len : ", len(polygn_price_samples))
+    # Generate stochastic process realizations
+    ## Linear
+    polygn_price_samples = create_stochastic_process_realizations(
+        "convex_polygn_price_samples", timesteps=TIMESTEPS, dt=DELTA_TIME
+    )
+    adoption_rates_slow = create_stochastic_process_realizations(
+        "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=500
+    )
+    adoption_rates_med = create_stochastic_process_realizations(
+        "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=2000
+    )
+    adoption_rates_fast = create_stochastic_process_realizations(
+        "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=3500
+    )
+    adoption_rates_public_slow = create_stochastic_process_realizations(
+        "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=5
+    )
+    adoption_rates_public_med = create_stochastic_process_realizations(
+        "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=15
+    )
+    adoption_rates_public_fast = create_stochastic_process_realizations(
+        "adoption_rates", timesteps=TIMESTEPS, dt=DELTA_TIME, final_chains_num=25
+    )
+    hardware_cost_moores_law = create_stochastic_process_realizations(
+        "hardware_costs", timesteps=TIMESTEPS, dt=DELTA_TIME, init_hardware_cost=500
+    )
 
-parameter_overrides = {
-    "polygn_price_process": [
-        lambda run, timestep: polygn_price_samples[run - 1][timestep],
-    ],
-    "Adoption_speed_process": [
-        lambda run, timestep: adoption_rates_slow[run - 1][timestep],
-        lambda run, timestep: adoption_rates_med[run - 1][timestep],
-        lambda run, timestep: adoption_rates_fast[run - 1][timestep],
-    ],
-    "Adoption_speed_public_process": [
-        lambda run, timestep: adoption_rates_public_slow[run - 1][timestep],
-        lambda run, timestep: adoption_rates_public_med[run - 1][timestep],
-        lambda run, timestep: adoption_rates_public_fast[run - 1][timestep],
-    ],
-    "validator_hardware_costs_per_month_process": [
-        lambda run, timestep: hardware_cost_moores_law[run - 1][timestep],
-    ],
-}
+    print("End of generate_stochastic_processes...")
+    elapsed_time = time.time() - start_time
+    print(f"Process completed in {elapsed_time:.2f} seconds")
 
-# Override default experiment Simulation and System Parameters related to timing
-experiment.simulations[0].timesteps = TIMESTEPS
-print("TIMESTEPS : ", TIMESTEPS)
-experiment.simulations[0].model.params.update({"dt": [DELTA_TIME]})
+    return (
+        polygn_price_samples,
+        adoption_rates_slow,
+        adoption_rates_med,
+        adoption_rates_fast,
+        adoption_rates_public_slow,
+        adoption_rates_public_med,
+        adoption_rates_public_fast,
+        hardware_cost_moores_law,
+    )
 
-# Override default experiment System Parameters
-experiment.simulations[0].model.params.update(parameter_overrides)
+
+def get_parameter_overrides(
+    polygn_price_samples,
+    adoption_rates_slow,
+    adoption_rates_med,
+    adoption_rates_fast,
+    adoption_rates_public_slow,
+    adoption_rates_public_med,
+    adoption_rates_public_fast,
+    hardware_cost_moores_law,
+):
+    parameter_overrides = {
+        "polygn_price_process": [
+            lambda run, timestep: polygn_price_samples[run - 1][timestep],
+        ],
+        "Adoption_speed_process": [
+            lambda run, timestep: adoption_rates_slow[run - 1][timestep],
+            lambda run, timestep: adoption_rates_med[run - 1][timestep],
+            lambda run, timestep: adoption_rates_fast[run - 1][timestep],
+        ],
+        "Adoption_speed_public_process": [
+            lambda run, timestep: adoption_rates_public_slow[run - 1][timestep],
+            lambda run, timestep: adoption_rates_public_med[run - 1][timestep],
+            lambda run, timestep: adoption_rates_public_fast[run - 1][timestep],
+        ],
+        "validator_hardware_costs_per_month_process": [
+            lambda run, timestep: hardware_cost_moores_law[run - 1][timestep],
+        ],
+    }
+    return parameter_overrides
--- pages/No_Rewards_Policy.py
+++ pages/No_Rewards_Policy.py
@@ -1,43 +1,95 @@
 import streamlit as st
+import time
 import copy
+import importlib
 
-import experiments.visualizations as visualizations
-import new_economic as new_adoption
+import model.constants as constants
 from experiments.run import run
+import experiments.visualizations as visualizations
+
+import new_economic as new_adoption
+
+start_time = time.time()
 
 ### Main Window
-
+st.write("#### No Rewards Policy")
 st.markdown(
     "- **No Rewards Policy** : Validators receive only the chain fees and the 2% emissions as rewards. The token economy operates without additional incentives, allowing for natural market movements with no fixed guarantee on APY."
 )
 
-simulation_1 = copy.deepcopy(new_adoption.experiment.simulations[0])
+with st.spinner(text="In progress..."):
 
-# st.write(simulation_1.model.params)
+    DELTA_TIME = constants.epochs_per_week  # epochs per timestep
+    SIMULATION_TIME_DAYS = 5 * 365  # number of months
+    TIMESTEPS = constants.epochs_per_day * SIMULATION_TIME_DAYS // DELTA_TIME
 
-simulation_1.model.params.update(
-    {"PRIVATE_CHAINS_CNT": [0], "PUBLIC_CHAINS_CNT": [0], "slashing_fraction": [0]}
-)
+    # generate stochastic processes
+    (
+        polygn_price_samples,
+        adoption_rates_slow,
+        adoption_rates_med,
+        adoption_rates_fast,
+        adoption_rates_public_slow,
+        adoption_rates_public_med,
+        adoption_rates_public_fast,
+        hardware_cost_moores_law,
+    ) = new_adoption.generate_stochastic_processes(TIMESTEPS, DELTA_TIME)
 
-df_1, _exceptions = run(simulation_1)
+    # get parameter overrides
+    parameter_overrides = new_adoption.get_parameter_overrides(
+        polygn_price_samples,
+        adoption_rates_slow,
+        adoption_rates_med,
+        adoption_rates_fast,
+        adoption_rates_public_slow,
+        adoption_rates_public_med,
+        adoption_rates_public_fast,
+        hardware_cost_moores_law,
+    )
 
-# plot the no of public chains per subset
-fig1 = visualizations.plot_number_of_public_chains_per_subset(
-    df_1, scenario_names={0: "Low Adoption", 1: "Medium Adoption", 2: "High Adoption"}
-)
+    # store this simulation as no_rewards_simulation
+    no_rewards_simulation = copy.deepcopy(new_adoption.experiment.simulations[0])
+    print("no_rewards_simulation : ", no_rewards_simulation)
 
-st.plotly_chart(fig1)
+    # override default experiment Simulation and System Parameters related to timing
+    no_rewards_simulation.timesteps = TIMESTEPS
+    print("TIMESTEPS : ", TIMESTEPS)
+    no_rewards_simulation.model.params.update({"dt": [DELTA_TIME]})
 
-# plot yields per subset subplots
-fig2 = visualizations.plot_yields_per_subset_subplots(
-    df_1, scenario_names={0: "Low Adoption", 1: "Medium Adoption", 2: "High Adoption"}
-)
+    # override default experiment System Parameters
+    no_rewards_simulation.model.params.update(parameter_overrides)
 
-st.plotly_chart(fig2)
+    no_rewards_simulation.model.params.update(
+        {"PRIVATE_CHAINS_CNT": [0], "PUBLIC_CHAINS_CNT": [0], "slashing_fraction": [0]}
+    )
 
-# plot the treasury balance barplot
-fig3 = visualizations.plot_treasury_balance_barplot(
-    df_1,
-)
+    df_1, _exceptions = run(no_rewards_simulation)
 
-st.plotly_chart(fig3)
+    # plot the no of public chains per subset
+    fig1 = visualizations.plot_number_of_public_chains_per_subset(
+        df_1,
+        scenario_names={0: "Low Adoption", 1: "Medium Adoption", 2: "High Adoption"},
+    )
+
+    st.plotly_chart(fig1)
+
+    # plot yields per subset subplots
+    fig2 = visualizations.plot_yields_per_subset_subplots(
+        df_1,
+        scenario_names={0: "Low Adoption", 1: "Medium Adoption", 2: "High Adoption"},
+    )
+
+    st.plotly_chart(fig2)
+
+    # plot the treasury balance barplot
+    fig3 = visualizations.plot_treasury_balance_barplot(
+        df_1,
+    )
+
+    st.plotly_chart(fig3)
+
+if st.button("Back Home"):
+    st.switch_page("Babylon_Growth_Analysis.py")
+
+elapsed_time = time.time() - start_time
+st.success(f"Process completed in {elapsed_time:.2f} seconds")
